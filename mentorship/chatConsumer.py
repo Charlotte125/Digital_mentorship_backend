a188045e7d123fe34b30ee4c104bf434454253f1
karera-o <olivierkarera2020@gmail.com>
2025-01-15 21:30:33 +0200

    (Requirements): refactoring the requirements

--- mentorship/chatConsumer.py
+++ mentorship/chatConsumer.py
@@ -1,40 +1,93 @@
+# from channels.generic.websocket import AsyncWebsocketConsumer
+# import json
+
+# class ChatConsumer(AsyncWebsocketConsumer):
+
+#     async def connect(self):
+
+#         await self.accept()
+
+#     async def receive(self,text_data):
+
+#         message = json.loads(text_data)
+#         message_type = message['message_type']
+
+#         if message_type == "clients":
+#             await self.client_message_handler(message)
+        
+#         elif message_type == "employees":
+#             await self.employee_message_handler(message)
+
+#     async def disconnect(self,close_code):
+#         print("The Connection is closed")
+
+
+#     async def client_message_handler(self,data):
+
+#         message ={
+#             "message": "We Love Our Client",
+#             "data": data["message"]
+#         }
+
+#         await self.send(text_data=json.dumps(message))
+
+#     async def employee_message_handler(self,data):
+#         message ={
+#             "message": "We Love Our Employees",
+#             "data": data["message"]
+#         }
+        
+#         await self.send(text_data=json.dumps(message))
+
+# consumers.py
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 
 class ChatConsumer(AsyncWebsocketConsumer):
-
     async def connect(self):
+        # Capture the room_id from the URL
+        # self.room_id = self.scope['url_route']['kwargs']['room_id']
+        
+        # Create a group name using the room_id to distinguish between rooms
+        # self.room_group_name = f"chat_{self.room_id}"
+        self.room_group_name = "testing_room"
+        print(self.room_group_name)
 
+        # Join the room group
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        # Accept the WebSocket connection
         await self.accept()
 
-    async def receive(self,text_data):
+    async def disconnect(self, close_code):
+        # Leave the room group when the WebSocket disconnects
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
 
-        message = json.loads(text_data)
-        message_type = message['message_type']
+    async def receive(self, text_data):
+        # Handle incoming message and send it to the group
+        text_data_json = json.loads(text_data)
+        message = text_data_json.get('message', '')
 
-        if message_type == "clients":
-            await self.client_message_handler(message)
+        # Send the message to the room group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
         
-        elif message_type == "employees":
-            await self.employee_message_handler(message)
 
-    async def disconnect(self,close_code):
-        print("The Connection is closed")
-
-
-    async def client_message_handler(self,data):
-
-        message ={
-            "message": "We Love Our Client",
-            "data": data["message"]
-        }
-
-        await self.send(text_data=json.dumps(message))
-
-    async def employee_message_handler(self,data):
-        message ={
-            "message": "We Love Our Employees",
-            "data": data["message"]
-        }
-        
-        await self.send(text_data=json.dumps(message))
+    # Receive message from room group
+    async def chat_message(self, event):
+        # Send the message to WebSocket
+        message = event['message']
+        await self.send(text_data=json.dumps({
+            'message': message
+        }))
